aaa2c45805e1992575304e34bd3444ffd57403fc
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-10-18 14:06:12 -0400

    tailwind added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return <>
     <h1>Home</h1>
-    <p>Put something interesting here!</p>
+    <p className="mt-20">Put something interesting here!</p>
   </>
 }
